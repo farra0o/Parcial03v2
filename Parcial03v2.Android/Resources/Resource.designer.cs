537782eddbf11beccb101e83edfd933c0ae3cfa2
farra0o <rr_072@hotmail.com>
2021-05-06 20:22:52 -0600

    Finish

--- Parcial03v2.Android/Resources/Resource.designer.cs
+++ Parcial03v2.Android/Resources/Resource.designer.cs
@@ -15489,19 +15489,22 @@ namespace Parcial03v2.Droid
 			public const int Per01 = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int Profile01 = 2131165341;
+			public const int Profile0 = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int test_custom_background = 2131165342;
+			public const int Profile01 = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int tooltip_frame_dark = 2131165343;
+			public const int test_custom_background = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int tooltip_frame_light = 2131165344;
+			public const int tooltip_frame_dark = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int xamarin_logo = 2131165345;
+			public const int tooltip_frame_light = 2131165345;
+			
+			// aapt resource value: 0x7F0700A2
+			public const int xamarin_logo = 2131165346;
 			
 			static Drawable()
 			{
